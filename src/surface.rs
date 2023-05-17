014509f32c1b46ff3c7a19ccc8dabeda2ef2ba9f
Kyle M. Douglass <kyle.m.douglass@gmail.com>
2023-05-17 11:56:18 +0200

    Add radius field and rename RefractingSurface to RefrCircSurf

--- src/surface.rs
+++ src/surface.rs
@@ -3,18 +3,21 @@ use crate::vec3::Vec3;
 type RefractiveIndex = fn(f32) -> (f32, f32);
 type SagNorm = fn(Vec3) -> (f32, Vec3);
 
-struct RefractingSurface {
+/// A refracting surface with circular cross-section.
+struct RefrCircSurf {
     pos: Vec3,
     dir: Vec3,
+    radius: f32,
     n: RefractiveIndex,
     sag_norm: SagNorm,
 }
 
-impl RefractingSurface {
-    fn new(pos: Vec3, dir: Vec3, n: RefractiveIndex, sag_norm: SagNorm) -> Self {
+impl RefrCircSurf {
+    fn new(pos: Vec3, dir: Vec3, radius: f32, n: RefractiveIndex, sag_norm: SagNorm) -> Self {
         Self {
             pos,
             dir,
+            radius,
             n,
             sag_norm,
         }
@@ -22,7 +25,7 @@ impl RefractingSurface {
 }
 
 enum Surface {
-    Refracting(RefractingSurface),
+    RefrCircSurf(RefrCircSurf),
 }
 
 #[cfg(test)]
@@ -33,9 +36,10 @@ mod tests {
     fn test_refracting_surface() {
         let pos = Vec3::new(0.0, 0.0, 0.0);
         let dir = Vec3::new(0.0, 0.0, 1.0);
+        let radius = 1.0;
         let n = |z| (1.0, 1.0);
         let sag_norm = |pos| (0.0, Vec3::new(0.0, 0.0, 1.0));
-        let surface = RefractingSurface::new(pos, dir, n, sag_norm);
+        let surface = RefrCircSurf::new(pos, dir, radius, n, sag_norm);
         assert_eq!(surface.pos, pos);
         assert_eq!(surface.dir, dir);
     }
