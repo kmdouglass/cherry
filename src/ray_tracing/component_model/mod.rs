1dd935c86de71d042f0203b1d119c8594c78cbd4
Kyle Douglass <kyle.m.douglass@gmail.com>
2023-08-06 10:12:16 +0200

    Implemented basic ComponentModel behavior

--- src/ray_tracing/component_model/mod.rs
+++ src/ray_tracing/component_model/mod.rs
@@ -1,8 +1,41 @@
 use std::collections::HashSet;
 
 /// A part is a surface, gap, surface set.
-pub struct Part {}
+/// 
+/// To avoid copying data, only indexes are stored.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Component {
+    surf_idx_1: usize,
+    gap_idx: usize,
+    surf_idx_2: usize,
+}
+
+impl Component {
+    pub fn new(surf_idx_1: usize, gap_idx: usize, surf_idx_2: usize) -> Self {
+        Self {
+            surf_idx_1,
+            gap_idx,
+            surf_idx_2,
+        }
+    }
+}
 
 struct ComponentModel {
-    parts: HashSet<Part>,
+    components: HashSet<Component>,
+}
+
+impl ComponentModel {
+    pub fn new() -> Self {
+        Self {
+            components: HashSet::new(),
+        }
+    }
+
+    pub fn insert_component(&mut self, component: Component) {
+        self.components.insert(component);
+    }
+
+    pub fn remove_component(&mut self, component: Component) {
+        self.components.remove(&component);
+    }
 }
