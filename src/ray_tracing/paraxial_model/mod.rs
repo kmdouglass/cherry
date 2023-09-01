3879c6b8eacdae4a9a3e06d5f07d29dd413b287d
Kyle Douglass <kyle.m.douglass@gmail.com>
2023-09-01 09:34:33 +0200

    WIP Add function to compute paraxial images of all surfaces in a system

--- src/ray_tracing/paraxial_model/mod.rs
+++ src/ray_tracing/paraxial_model/mod.rs
@@ -1,4 +1,12 @@
+use crate::ray_tracing::Surface;
+
 /// Find the paraxial image distance from a surface or thin lens.
-pub fn img_dist(obj_dist: f32, focal_length: f32) -> f32 {
+fn img_dist(obj_dist: f32, focal_length: f32) -> f32 {
     1.0 / (1.0 / focal_length - 1.0 / obj_dist)
+}
+
+/// Find the paraxial image locations of a sequence of surfaces in image space.
+pub fn img_space_surf_locs(surfs: &[Surface]) -> Vec<f32> {    
+    // TODO: Implement this function.
+    Vec::new()
 }
