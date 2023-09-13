5c398614f6e88439b158d104dfcb72612e4867a2
David Wagner <wagdav@gmail.com>
2023-09-13 22:32:56 +0200

    Fix URL

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,5 +3,5 @@
 set -eu
 
 SITE=$(nix build .#site --no-link --print-out-paths)
-nix run nixpkgs#ghp-import -- --message "Automatic update from https://github.com/wagdav/cherry-web" "$SITE"
+nix run nixpkgs#ghp-import -- --message "Automatic update from https://github.com/kmdouglass/cherry" "$SITE"
 git push --force origin gh-pages:gh-pages
