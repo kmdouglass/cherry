2d0fce3fbca17bf2d3ef50f6a0402a66f0e9d8f0
Kyle Douglass <kyle.m.douglass@gmail.com>
2024-01-19 11:23:37 +0100

    Remove bootstrap.js and manually initialize Wasm module

--- www/js/index.js
+++ www/js/index.js
@@ -1,27 +1,29 @@
-import { WasmSystemModel } from "cherry-js";
+import init, { WasmSystemModel } from "./pkg/cherry_js.js";
 import { renderSystem } from "./modules/rendering.js"
 //import { surfaces, gaps, aperture, fields } from "./modules/petzval_lens.js";
 import { surfaces, gaps, aperture, fields } from "./modules/planoconvex_lens.js";
 
-let wasmSystemModel = new WasmSystemModel();
+init().then(() => {
+    let wasmSystemModel = new WasmSystemModel();
 
-//Build the optical system
-wasmSystemModel.setSurfaces(surfaces);
-wasmSystemModel.setGaps(gaps);
-wasmSystemModel.setAperture(aperture);
-wasmSystemModel.setFields(fields);
-wasmSystemModel.build();
+    //Build the optical system
+    wasmSystemModel.setSurfaces(surfaces);
+    wasmSystemModel.setGaps(gaps);
+    wasmSystemModel.setAperture(aperture);
+    wasmSystemModel.setFields(fields);
+    wasmSystemModel.build();
 
-let descr = wasmSystemModel.describe();
-console.log(descr);
+    let descr = wasmSystemModel.describe();
+    console.log(descr);
 
-// Render the system -- SVG
-renderSystem(wasmSystemModel);
+    // Render the system -- SVG
+    renderSystem(wasmSystemModel);
 
-// Perform the full ray trace
-const start = performance.now();
-const results = wasmSystemModel.trace();
-const end = performance.now();
-console.log(`Full ray trace took ${end - start} milliseconds.`);
+    // Perform the full ray trace
+    const start = performance.now();
+    const results = wasmSystemModel.trace();
+    const end = performance.now();
+    console.log(`Full ray trace took ${end - start} milliseconds.`);
 
-console.log(results);
+    console.log(results);
+});
