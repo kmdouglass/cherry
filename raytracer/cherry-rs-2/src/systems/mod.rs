4e46e334640fe9911bb4167beb542073cd362d11
Kyle Douglass <kyle.m.douglass@gmail.com>
2024-04-03 13:34:36 +0200

    Compute paraxial model IDs from wavelengths and axis

--- raytracer/cherry-rs-2/src/systems/mod.rs
+++ raytracer/cherry-rs-2/src/systems/mod.rs
@@ -3,6 +3,15 @@ use anyhow::{anyhow, Result};
 use crate::core::Float;
 use crate::specs::{fields::FieldSpec, gaps::GapSpec};
 
+type ParaxialModelID = (usize, Axis);
+
+/// The transverse direction along which system properties will be computed.
+#[derive(Debug, Clone, Copy)]
+enum Axis {
+    Horizontal,
+    Vertical,
+}
+
 #[derive(Debug)]
 pub struct SeqSystem {
     fields: Vec<FieldSpec>,
@@ -16,6 +25,20 @@ pub struct SeqSysBuilder {
     fields: Option<Vec<FieldSpec>>,
     gaps: Option<Vec<GapSpec>>,
     wavelengths: Option<Vec<Float>>,
+}
+
+impl SeqSystem {
+    /// Computes the unique IDs for each paraxial model.
+    fn paraxial_model_ids(&self) -> Vec<ParaxialModelID> {
+        let mut ids = Vec::new();
+        for (idx, _wavelength) in self.wavelengths.iter().enumerate() {
+            for axis in [Axis::Horizontal, Axis::Vertical].iter() {
+                let id = (idx, *axis);
+                ids.push(id);
+            }
+        }
+        ids
+    }
 }
 
 impl SeqSysBuilder {
@@ -55,7 +78,7 @@ impl SeqSysBuilder {
             .ok_or(anyhow!("The system's gaps must be specified."))?;
         let wavelengths = self
             .wavelengths
-            .ok_or(anyhow!("The system's wavelenghts must be specified."))?;
+            .ok_or(anyhow!("The system's wavelengths must be specified."))?;
 
         Ok(SeqSystem {
             fields,
