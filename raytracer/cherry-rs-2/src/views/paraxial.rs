7f961338e4857bff186114f196ae5ab5da01282e
Kyle Douglass <kyle.m.douglass@gmail.com>
2024-06-18 16:28:26 +0200

    Return ndarray object for ParaxialRayTraceResults

--- raytracer/cherry-rs-2/src/views/paraxial.rs
+++ raytracer/cherry-rs-2/src/views/paraxial.rs
@@ -2,10 +2,19 @@
 
 use std::cell::OnceCell;
 
+use ndarray::Array3;
+
 use crate::core::Float;
 
+/// A Ns x Nr x 2 array of ray trace results.
+///
+/// Ns is the number of surfaces, and Nr is the number of rays. The first column is the
+/// height of the ray at the surface, and the second column is the angle of the ray at the
+/// surface.
+type ParaxialRayTraceResults = Array3<Float>;
+
 pub struct ParaxialView {
-    pseudo_marginal_ray: OnceCell<Float>,
+    pseudo_marginal_ray: OnceCell<ParaxialRayTraceResults>,
 }
 
 impl ParaxialView {
@@ -17,7 +26,10 @@ impl ParaxialView {
     }
 
     /// Compute the pseudo marginal ray.
-    pub fn pseudo_marginal_ray(&self) -> &Float {
-        self.pseudo_marginal_ray.get_or_init(|| 0.0)
+    pub fn pseudo_marginal_ray(&self) -> &ParaxialRayTraceResults {
+        self.pseudo_marginal_ray.get_or_init(|| {
+            let ray_trace_results = Array3::zeros((1, 1, 2));
+            ray_trace_results
+        })
     }
 }
