1cd459b2c14b2469ccad25bfd7e0e4975cf92318
Kyle Douglass <kyle.m.douglass@gmail.com>
2024-08-27 09:39:27 +0200

    Make cells editable

--- www/js/src/components/SurfacesTable.js
+++ www/js/src/components/SurfacesTable.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import "../css/SurfacesTable.css";
+
 const SurfacesTable = () => {
     const [surfaces, setSurfaces] = useState([
       { type: 'Object Plane', n: 1, thickness: 'Infinity', diam: 25, roc: '', actions: 'Insert' },
@@ -7,6 +9,8 @@ const SurfacesTable = () => {
       { type: 'Conic', n: 1, thickness: 46.6, diam: 25, roc: Infinity, actions: 'Insert Delete' },
       { type: 'Image Plane', n: '', thickness: '', diam: 25, roc: '', actions: '' },
     ]);
+
+    const [editingCell, setEditingCell] = useState(null);
 
     const getSurfaceTypeDefaultValues = (type) => {
         switch (type) {
@@ -32,6 +36,27 @@ const SurfacesTable = () => {
         setSurfaces(newSurfaces);
     }
 
+    const handleCellClick = (index, field) => {
+        if (index === surfaces.length - 1) return; // Don't allow editing the last row
+        setEditingCell({ index, field });
+    };
+    
+    const handleCellChange = (e, index, field) => {
+        const newSurfaces = [...surfaces];
+        newSurfaces[index][field] = e.target.value;
+        setSurfaces(newSurfaces);
+    };
+    
+    const handleCellBlur = () => {
+        setEditingCell(null);
+    };
+    
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+          setEditingCell(null);
+        }
+    };
+
     const renderSurfaceTypeCell = (surface, index) => {
         if (index === 0) {
           return <td>Object</td>;
@@ -54,6 +79,31 @@ const SurfacesTable = () => {
           </td>
         );
     };
+
+    const renderEditableCell = (value, index, field) => {
+        if (editingCell && editingCell.index === index && editingCell.field === field) {
+        return (
+            <div className="editable-cell">
+                <span>{value}</span>
+                <input
+                    type="text"
+                    value={value}
+                    onChange={(e) => handleCellChange(e, index, field)}
+                    onBlur={handleCellBlur}
+                    onKeyDown={handleKeyDown}
+                    autoFocus
+                />
+            </div>
+        );
+        }
+        return (
+            <div className="editable-cell">
+                <span onClick={() => handleCellClick(index, field)}>
+                {value}
+                </span>
+            </div>
+        );
+    };
   
     return (
       <table className="table is-fullwidth">
@@ -71,10 +121,10 @@ const SurfacesTable = () => {
           {surfaces.map((surface, index) => (
             <tr key={index}>
               {renderSurfaceTypeCell(surface, index)}
-              <td>{surface.n}</td>
-              <td>{surface.thickness}</td>
-              <td>{surface.diam}</td>
-              <td>{surface.roc}</td>
+              <td>{renderEditableCell(surface.n, index, "n")}</td>
+              <td>{renderEditableCell(surface.thickness, index, "thickness")}</td>
+              <td>{renderEditableCell(surface.diam, index, "diam")}</td>
+              <td>{renderEditableCell(surface.roc, index, "roc")}</td>
               <td>{surface.actions}</td>
             </tr>
           ))}
