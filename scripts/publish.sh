b831a361faa11f09d737ec6db3af0818eb1d6efb
Kyle Douglass <kyle.m.douglass@gmail.com>
2024-09-30 08:53:00 +0200

    Remove now unused build files

--- scripts/publish.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-set -eu
-
-SITE=$(nix build .#site --no-link --print-out-paths)
-nix run nixpkgs#ghp-import -- --message "Automatic update from https://github.com/kmdouglass/cherry" "$SITE"
-git push --force origin gh-pages:gh-pages
