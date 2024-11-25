080a9d70a17bdb2869f5ddefcd9a228c5b2be451
Kyle M. Douglass <kyle.m.douglass@gmail.com>
2024-11-25 12:01:41 +0100

    Remove CLJS from repo

--- www/cljs/src/main/net/thewagner/build.clj
+++
@@ -1,9 +0,0 @@
-(ns net.thewagner.build
-  (:require [hiccup.page :refer [html5]]
-            [net.thewagner.html :as pages]))
-
-(defn hook
-  {:shadow.build/stage :flush}
-  [{:keys [shadow.build/config] :as build-state} & _args]
-  (spit (str (:output-dir config) "/../index.html") (html5 pages/index-head pages/index-body))
-  build-state)
