d10a13d7fa9f749736c947386001adabd68266cb
Kyle Douglass <kyle.m.douglass@gmail.com>
2025-08-12 15:13:04 +0200

    Add normalize method for Vec2

--- raytracer/cherry-rs/src/core/math/vec2.rs
+++ raytracer/cherry-rs/src/core/math/vec2.rs
@@ -1,12 +1,26 @@
 /// A 2D vector.
 use std::ops::Index;
 
-use crate::core::Float;
+use crate::core::{Float, math::constants::ZERO_TOL};
 
 #[derive(Debug)]
 pub struct Vec2 {
     pub x: Float,
     pub y: Float,
+}
+
+impl Vec2 {
+    // Normalizes the vector in place.
+    //
+    // This method modifies the vector to have a length of 1 if the length is greater
+    // than a small tolerance near zero. Otherwise, it is unchanged.
+    pub fn normalize(&mut self) {
+        let length = (self.x * self.x + self.y * self.y).sqrt();
+        if length > ZERO_TOL {
+            self.x /= length;
+            self.y /= length;
+        }
+    }
 }
 
 impl Index<usize> for Vec2 {
@@ -18,5 +32,19 @@ impl Index<usize> for Vec2 {
             1 => &self.y,
             _ => panic!("Index out of bounds for Vec2"),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::core::math::constants::REL_TOL;
+    use super::*;
+
+    #[test]
+    fn test_normalize() {
+        let mut vec = Vec2 { x: 3.0, y: 4.0 };
+        vec.normalize();
+        assert!((vec.x - 0.6).abs() < REL_TOL);
+        assert!((vec.y - 0.8).abs() < REL_TOL);
     }
 }
