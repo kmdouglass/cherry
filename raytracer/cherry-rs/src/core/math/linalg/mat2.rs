365b2e42022bb416161a792d6fcc46b853ed3712
Kyle Douglass <kyle.m.douglass@gmail.com>
2025-08-12 11:10:58 +0200

    Add Vec2 type

--- raytracer/cherry-rs/src/core/math/linalg/mat2.rs
+++ raytracer/cherry-rs/src/core/math/linalg/mat2.rs
@@ -1,26 +1,42 @@
-/// A 2 x 2 matrix
-use crate::core::Float;
+/// A 2 x 2 matrix.
+use std::ops::Index;
+
+use crate::core::{Float, math::vec2::Vec2};
 
 #[derive(Debug)]
 pub struct Mat2 {
-    pub e: [[Float; 2]; 2],
+    col_1: Vec2,
+    col_2: Vec2,
 }
 
 impl Mat2 {
     pub fn new(e00: Float, e01: Float, e10: Float, e11: Float) -> Self {
         Self {
-            e: [[e00, e01], [e10, e11]],
+            col_1: Vec2 { x: e00, y: e10 },
+            col_2: Vec2 { x: e01, y: e11 },
         }
     }
 
     /// Computes the determinant of the matrix.
     pub fn determinant(&self) -> Float {
-        self.e[0][0] * self.e[1][1] - self.e[0][1] * self.e[1][0]
+        self.col_1.x * self.col_2.y - self.col_1.y * self.col_2.x
     }
 
     /// Computes the trace of the matrix.
     pub fn trace(&self) -> Float {
-        self.e[0][0] + self.e[1][1]
+        self.col_1.x + self.col_2.y
+    }
+}
+
+impl Index<usize> for Mat2 {
+    type Output = Vec2;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        match index {
+            0 => &self.col_1,
+            1 => &self.col_2,
+            _ => panic!("Index out of bounds for Mat2"),
+        }
     }
 }
 
@@ -31,10 +47,10 @@ mod test {
     #[test]
     fn mat2_new() {
         let m = Mat2::new(1.0, 2.0, 3.0, 4.0);
-        assert_eq!(m.e[0][0], 1.0);
-        assert_eq!(m.e[0][1], 2.0);
-        assert_eq!(m.e[1][0], 3.0);
-        assert_eq!(m.e[1][1], 4.0);
+        assert_eq!(m[0][0], 1.0);
+        assert_eq!(m[0][1], 2.0);
+        assert_eq!(m[1][0], 3.0);
+        assert_eq!(m[1][1], 4.0);
     }
 
     #[test]
