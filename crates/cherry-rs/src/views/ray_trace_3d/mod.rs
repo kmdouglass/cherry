1411e56349c3daa71357cdca6c8635ad441bad94
Kyle Douglass <kyle.m.douglass@gmail.com>
2026-03-24 10:16:34 +0100

    Add justfile with trace command

--- crates/cherry-rs/src/views/ray_trace_3d/mod.rs
+++ crates/cherry-rs/src/views/ray_trace_3d/mod.rs
@@ -108,6 +108,13 @@ pub fn ray_trace_3d_view(
     for (field_id, wavelength_id, axis) in combinations {
         let _trace_bundle_span = trace_span!("trace_bundle", field_id, wavelength_id).entered();
 
+        tracing::trace!(
+            "Tracing rays for field_id={}, wavelength_id={}, axis={:?}",
+            field_id,
+            wavelength_id,
+            axis
+        );
+
         let submodel_id = SubModelID(wavelength_id, axis);
         let fallback_id = SubModelID(wavelength_id, Axis::U);
         let sequential_submodel = sequential_model
