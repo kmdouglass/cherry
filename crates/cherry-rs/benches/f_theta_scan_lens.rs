2f04e0578f7c59c8deae2b11c3780c93e716ca27
Kyle M. Douglass <kyle.m.douglass@gmail.com>
2026-04-09 13:44:52 +0200

    fix: Use non-normalized surface normals in Newton-Raphson updates (#336)
    The Surface::sag_norm method was returning unit, a.k.a. normalized, surface normal vectors which had the effect of modifying the denominator term in the Newton-Raphson update step and causing oscillations at angles of incidence greater than ~10 degrees on highly curved surfaces.
    
    Now, the magnitude of the normal vectors is retained and convergence has improved such that there is a 40% speed improvement in the convexplano lens and the f-theta scan lens no longer has convergence failures.
    
    I also added a fallback branch of the NR routine that uses a bisection method when sag is NaN to bring the ray back into a valid numerical regime.
    
    Finally, the NR routine now terminates early if a ray is terminated. This originally caused a slight increase in the benchmark tests due to branch prediction failures, but it was more than compensated for by improving the convergence rate and reducing the error rate in individual ray/surface intersection calculations.

---
+++ crates/cherry-rs/benches/f_theta_scan_lens.rs
@@ -0,0 +1,40 @@
+use criterion::{Criterion, criterion_group, criterion_main};
+use std::hint::black_box;
+
+use cherry_rs::{
+    ApertureSpec, Axis, FieldSpec, ParaxialView, PupilSampling,
+    examples::f_theta_scan_lens::sequential_model, n, ray_trace_3d_view,
+};
+
+const WAVELENGTHS: [f64; 1] = [0.5876]; // He d line
+const APERTURE_SPEC: ApertureSpec = ApertureSpec::EntrancePupil { semi_diameter: 0.5 };
+
+fn benchmark(c: &mut Criterion) {
+    let model = sequential_model(n!(1.0), n!(1.84666), &WAVELENGTHS);
+    let field_specs = vec![FieldSpec::Angle {
+        angle: 20.0,
+        pupil_sampling: PupilSampling::TangentialRayFan {
+            n: 9,
+            axis: Axis::U,
+        },
+    }];
+    let paraxial_view = ParaxialView::new(&model, &field_specs, false).unwrap();
+    let mut group = c.benchmark_group("3D ray trace, f-theta scan lens");
+
+    group.bench_function("ray_trace_3d_view, 20 deg off-axis", |b| {
+        b.iter(|| {
+            ray_trace_3d_view(
+                black_box(&APERTURE_SPEC),
+                black_box(&field_specs),
+                black_box(&model),
+                black_box(&paraxial_view),
+                black_box(None),
+            )
+            .unwrap();
+        });
+    });
+    group.finish();
+}
+
+criterion_group!(benches, benchmark);
+criterion_main!(benches);
--- justfile
+++ justfile
@@ -1,3 +1,16 @@
+[doc("Run a benchmark test. Example: 'just bench f_theta_scan_lens'")]
+[working-directory: "crates"]
+bench bench_name:
+  cargo bench --bench {{bench_name}}
+
+[working-directory: "crates"]
+list-benches:
+  cargo bench -- --list
+
+[working-directory: "crates"]
+list-tests:
+  cargo test --test '*' -- --list
+
 [doc("Run an integration test with tracing. Filter example: '[{ray_id=0}]=trace'")]
 [arg("filter", long, short="f")]
 [arg("test_name", long, short="n")]
